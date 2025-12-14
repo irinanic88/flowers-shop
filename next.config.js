2defc2f2b4d6f9f4162e77bf51744d622401ca95
inicolai01 <irina.nic.yu@gmail.com>
2025-12-14 17:24:21 +0100

    Update versions

---
+++ next.config.js
@@ -0,0 +1,14 @@
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "kxcfeoiafbezsgcpyzss.supabase.co",
+        pathname: "/storage/v1/object/public/**",
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
