6a5300b0186d6b2a98c34b6dc44a86dcd0d1a568
inicolai01 <irina.nic.yu@gmail.com>
2025-12-27 17:28:59 +0100

    Build linter fix 1

--- src/components/products/ProductsFilters.tsx
+++ src/components/products/ProductsFilters.tsx
@@ -1,15 +1,15 @@
-"use client";
+'use client';
 
-import React from "react";
+import React from 'react';
 import {
   Stack,
   Select,
   MenuItem,
   InputLabel,
   FormControl,
-} from "@mui/material";
-import { DisponibilityType, ProductType } from "@/src/types";
-import { availabilityStatusesDict } from "@/src/constants";
+} from '@mui/material';
+import { ProductType } from '@/src/types';
+import { availabilityStatusesDict } from '@/src/constants';
 
 interface ProductsFiltersProps {
   availabilityFilter: DisponibilityType | "all";
@@ -32,7 +32,7 @@ export function ProductsFilters({
         maxWidth: "100%",
       }}
     >
-      <FormControl size="small" variant="filled" sx={{ width: "100%" }}>
+      <FormControl size="small" variant="filled" sx={{ width: '100%' }}>
         <InputLabel>Disponibilidad</InputLabel>
         <Select
           value={availabilityFilter}
